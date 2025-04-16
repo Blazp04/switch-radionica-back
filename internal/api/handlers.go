c892cc7f357ed89612982d3113c5d93a78522316
blazp04 <blaz.peric4@gmail.com>
2025-04-16 15:25:19 +0200

    Disable cors

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -252,10 +252,7 @@ func SetupRouter(server *Server, jwtSecret string) *gin.Engine {
 	// Create a CORS middleware instance
 	corsMiddleware := cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:3000",
-			"https://radionica.blazperic.com",
-			"https://radionica-switch-front.vercel.app",
-			"https://test-radionica.vercel.app",
+			"*",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "ngrok-skip-browser-warning"},
